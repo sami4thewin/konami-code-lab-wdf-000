dd8d4dc37c5daf88e28b1f7861b104c2a379ef36
sami4thewin <samiomarmohammed@gmail.com>
2016-11-02 14:26:20 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+
+var body = document.body;
+
+
+
+
 function init() {
   // your code here
+  body.addEventListener('keydown', function(e) {
+    var key = parseInt(e.detail || e.which);
+
+    for(var i = 0; i < code.length; i++){
+      if (key === code[i]) {
+        alert("Congrats.")
+      }
+    }
+  })
+
 }
